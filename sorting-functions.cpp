74805ceb281292a07249df457f63ed561621fbb6
Davon Morris <88117514+davmor21@users.noreply.github.com>
2022-04-04 22:38:30 -0500

    Update sorting-functions.cpp
    Added SelectionSort() function

--- sorting-functions.cpp
+++ sorting-functions.cpp
@@ -23,3 +23,34 @@ int BinarySearch(vector<int>& myVec, int mySize, int searchValue){
    }
       return -1; // not found
 }
+
+   void SelectionSort(vector<int> numbers) {
+   int sizeVec = numbers.size();
+   int i;
+   int j;
+   int t;
+   int temp;
+   int indexLargest;
+   for(i = 0; i < sizeVec; ++i){
+        indexLargest = i;
+   for (j = i + 1; j < sizeVec; ++j) {
+
+      if (numbers[j] > numbers[indexLargest]) {
+         indexLargest = j;
+       }
+   }
+   temp = numbers[i];
+   numbers[i] = numbers[indexLargest];
+   numbers[indexLargest]= temp;
+   for( t = 0; t < sizeVec; ++t){
+          if(i == sizeVec - 1){
+              break; 
+           }
+          cout << numbers[t] << " " ; 
+               
+          if(t  == sizeVec - 1 ){
+              cout << endl;  
+           }
+       }
+      }
+   }
